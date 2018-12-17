30bc19e8a6d13420b61582b156cabd162353799f
Cc <chamcc-wa14@student.tarc.edu.my>
2018-12-17 08:55:42 +0800

    Initial

---
+++ app/src/main/java/my/edu/tarc/naviguide/MainActivity.java
@@ -0,0 +1,38 @@
+package my.edu.tarc.naviguide;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_discover:
+                    return true;
+                case R.id.navigation_search:
+                    startActivity(new Intent(MainActivity.this, SearchView.class));
+                    return true;
+                case R.id.navigation_profile:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
